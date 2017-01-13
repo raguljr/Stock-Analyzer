1581c7bfec66ec4d885acf3b59acea0934088d2a
raguljr <jothiraj.ragul@gmail.com>
2017-01-13 15:01:48 +0530

    Enhanced functionality

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,14 @@
 class WelcomeController < ApplicationController
   def index
+  	@comp=Company.all
+  end
+  def save
+  	company=Company.create(name: params[:v])
+  	
+  end
+
+  def part
+  	@com=Company.all
+  	render json: @com
   end
 end
